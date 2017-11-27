b8faf44da32921eab6af867d94951798dcf4f9f9
Anthony Gubler <agubler@sitepen.com>
2017-11-27 20:16:55 +0000

    format for the test app

--- test-app/src/App.ts
+++ test-app/src/App.ts
@@ -1,11 +1,11 @@
 import * as css from './app.m.css';
 
-const App = async function () {
-	const Foo = await import ('./Foo');
+const App = async function() {
+	const Foo = await import('./Foo');
 	return Foo.default;
 };
 
-App().then((result) => {
+App().then(result => {
 	console.log(result());
 	console.log(css);
 });
