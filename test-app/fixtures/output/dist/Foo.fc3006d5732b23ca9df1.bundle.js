6a459c03f7904a790a01642448021346dd9ea74a
Anthony Gubler <agubler@sitepen.com>
2017-11-28 12:51:48 +0000

    regenerate fixtures

--- test-app/fixtures/output/dist/Foo.fc3006d5732b23ca9df1.bundle.js
+++ test-app/fixtures/output/dist/Foo.fc3006d5732b23ca9df1.bundle.js
@@ -7,4 +7,4 @@
  * 
  */
 dojoWebpackJsonptest_app([0],{7:function(e,t,o){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.default=function(){return"foo"}}});
-//# sourceMappingURL=Foo.6bf1d49cff41619f4e23.bundle.js.map
+//# sourceMappingURL=Foo.fc3006d5732b23ca9df1.bundle.js.map
