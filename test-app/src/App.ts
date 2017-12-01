3504ac792a84ffb35351692c2be4fb471ddc1077
Anthony Gubler <agubler@sitepen.com>
2017-12-01 13:22:48 +0000

    Add manually configured bundling

--- test-app/src/App.ts
+++ test-app/src/App.ts
@@ -5,7 +5,21 @@ const App = async function() {
 	return Foo.default;
 };
 
-export default function() {
+const Bar = async function() {
+	const Bar = await import('./Bar');
+	return Bar.default;
+};
+
+const Baz = async function() {
+	const Baz = await import('./Baz');
+	return Baz.default;
+};
+
+export default async function() {
 	console.log(css);
+	const bar = await Bar();
+	const baz = await Baz();
+	bar();
+	baz();
 	return App();
 }
