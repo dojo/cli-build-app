fa1b8b5c107c82035eaf96850c64563e2ae4671e
Anthony Gubler <agubler@sitepen.com>
2017-12-05 14:36:50 +0000

    re organise the fixtures

--- tests/functional/main.ts
+++ tests/functional/main.ts
@@ -12,14 +12,14 @@ const appRootDir = path.join(__dirname, '..', '..', '..', 'test-app');
 const platform = os.platform().startsWith('win') ? 'windows' : 'unix';
 
 function assertOutput(mode: string) {
-	const fixtureManifest = require(path.join(appRootDir, 'fixtures', 'output', platform, mode, 'manifest'));
+	const fixtureManifest = require(path.join(appRootDir, 'fixtures', platform, mode, 'manifest'));
 	const outputManifest = require(path.join(appRootDir, 'output', mode, 'manifest'));
 	const fixtureFileIdentifiers = Object.keys(fixtureManifest);
 	const outputFileIdentifiers = Object.keys(outputManifest);
 	assert.deepEqual(outputFileIdentifiers, fixtureFileIdentifiers);
 	fixtureFileIdentifiers.forEach(id => {
 		if (id !== 'runtime.js.map') {
-			const fixtureFilePath = path.join(appRootDir, 'fixtures', 'output', platform, mode, fixtureManifest[id]);
+			const fixtureFilePath = path.join(appRootDir, 'fixtures', platform, mode, fixtureManifest[id]);
 			const outputFilePath = path.join(appRootDir, 'output', mode, outputManifest[id]);
 			let fixtureContents = fs.readFileSync(fixtureFilePath, 'utf8');
 			let outputContents = fs.readFileSync(outputFilePath, 'utf8');
