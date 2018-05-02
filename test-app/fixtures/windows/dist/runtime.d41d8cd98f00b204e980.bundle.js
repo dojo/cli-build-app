dccf345f376909f9610e5cdbf3a717986fd7c580
Anthony Gubler <agubler@sitepen.com>
2018-05-02 15:17:49 +0100

    Remove fixtures

--- test-app/fixtures/windows/dist/runtime.d41d8cd98f00b204e980.bundle.js
+++
@@ -1,9 +0,0 @@
-/*!
- * 
- * [Dojo](https://dojo.io/)
- * Copyright [JS Foundation](https://js.foundation/) & contributors
- * [New BSD license](https://github.com/dojo/meta/blob/master/LICENSE)
- * All rights reserved
- * 
- */!function(e){var n=window.dojoWebpackJsonptest_app;window.dojoWebpackJsonptest_app=function(r,a,c){for(var i,u,f,s=0,l=[];s<r.length;s++)u=r[s],t[u]&&l.push(t[u][0]),t[u]=0;for(i in a)Object.prototype.hasOwnProperty.call(a,i)&&(e[i]=a[i]);for(n&&n(r,a,c);l.length;)l.shift()();if(c)for(s=0;s<c.length;s++)f=o(o.s=c[s]);return f};var r={},t={runtime:0};function o(n){if(r[n])return r[n].exports;var t=r[n]={i:n,l:!1,exports:{}};return e[n].call(t.exports,t,t.exports,o),t.l=!0,t.exports}o.e=function(e){var n=t[e];if(0===n)return new Promise(function(e){e()});if(n)return n[2];var r=new Promise(function(r,o){n=t[e]=[r,o]});n[2]=r;var a=document.getElementsByTagName("head")[0],c=document.createElement("script");c.type="text/javascript",c.charset="utf-8",c.async=!0,c.timeout=12e4,o.nc&&c.setAttribute("nonce",o.nc),c.src=o.p+""+({main:"main",lazy:"lazy","src/Foo":"src/Foo",widgets:"widgets"}[e]||e)+"."+{main:"859205cbec3bf4c51f03",lazy:"87be983bbe3cb7281ada","src/Foo":"4ceb849f8bf6ae9d7e37",widgets:"030b4fa5d2b5ad5ff1f7"}[e]+".bundle.js";var i=setTimeout(u,12e4);function u(){c.onerror=c.onload=null,clearTimeout(i);var n=t[e];0!==n&&(n&&n[1](new Error("Loading chunk "+e+" failed.")),t[e]=void 0)}return c.onerror=c.onload=u,a.appendChild(c),r},o.m=e,o.c=r,o.d=function(e,n,r){o.o(e,n)||Object.defineProperty(e,n,{configurable:!1,enumerable:!0,get:r})},o.n=function(e){var n=e&&e.__esModule?function(){return e.default}:function(){return e};return o.d(n,"a",n),n},o.o=function(e,n){return Object.prototype.hasOwnProperty.call(e,n)},o.p="",o.oe=function(e){throw console.error(e),e}}([]);
-//# sourceMappingURL=runtime.d41d8cd98f00b204e980.bundle.js.map
