faddeac62bf80c6c396a9d75aa07edf32e116bdf
Matt Wistrand <matt.wistrand@gmail.com>
2018-08-06 14:11:23 -0500

    Include assets from "public/" directory
    Add a `copyPublicAssets` build flag used to determine whether files in
    an optional `public/` directory should be copied to the output
    directory.

--- src/dist.config.ts
+++ src/dist.config.ts
@@ -1,6 +1,7 @@
 import baseConfigFactory, { mainEntry, packageName } from './base.config';
 import { WebAppManifest } from './interfaces';
 import webpack = require('webpack');
+import * as fs from 'fs';
 import * as path from 'path';
 import { deepAssign } from '@dojo/framework/core/lang';
 import BuildTimeRender from '@dojo/webpack-contrib/build-time-render/BuildTimeRender';
@@ -10,6 +11,7 @@ import ServiceWorkerPlugin, {
 import * as HtmlWebpackPlugin from 'html-webpack-plugin';
 import * as ExtractTextPlugin from 'extract-text-webpack-plugin';
 import * as CleanWebpackPlugin from 'clean-webpack-plugin';
+import * as CopyWebpackPlugin from 'copy-webpack-plugin';
 import * as ManifestPlugin from 'webpack-manifest-plugin';
 import * as WebpackChunkHash from 'webpack-chunk-hash';
 
@@ -22,9 +24,16 @@ function webpackConfig(args: any): webpack.Configuration {
 	const manifest: WebAppManifest = args.pwa && args.pwa.manifest;
 	const serviceWorker: ServiceWorkerOptions = args.pwa && args.pwa.serviceWorker;
 	const { plugins, output } = config;
+	const { copyPublicAssets = true } = args;
+	const outputPath = path.join(output.path!, 'dist');
+	const publicDir = path.join(process.cwd(), 'public');
+	const publicDirExists = fs.existsSync(publicDir);
 
 	config.plugins = [
 		...plugins,
+		copyPublicAssets &&
+			publicDirExists &&
+			new CopyWebpackPlugin([{ from: path.join(process.cwd(), 'public'), to: path.join(outputPath, 'public') }]),
 		new ManifestPlugin(),
 		new BundleAnalyzerPlugin({
 			analyzerMode: 'static',
@@ -94,7 +103,7 @@ function webpackConfig(args: any): webpack.Configuration {
 
 	config.output = {
 		...output,
-		path: path.join(output.path!, 'dist'),
+		path: outputPath,
 		chunkFilename: '[name].[chunkhash].bundle.js',
 		filename: '[name].[chunkhash].bundle.js'
 	};
