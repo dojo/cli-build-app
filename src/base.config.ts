07e7a5869ced25cece250b1c3f75d6a84450c6ba
Tom Dye <github@tomdye.co.uk>
2018-09-26 10:52:36 +0100

    add rebase url plugin

--- src/base.config.ts
+++ src/base.config.ts
@@ -222,7 +222,13 @@ export default function webpackConfigFactory(args: any): WebpackConfiguration {
 				loader: 'postcss-loader?sourceMap',
 				options: {
 					ident: 'postcss',
-					plugins: [require('postcss-import')(), postcssPresetEnv(postcssPresetConfig)]
+					plugins: [
+						require('postcss-import')(),
+						require('postcss-url')({
+							url: 'rebase'
+						}),
+						postcssPresetEnv(postcssPresetConfig)
+					]
 				}
 			}
 		]
@@ -241,7 +247,13 @@ export default function webpackConfigFactory(args: any): WebpackConfiguration {
 				loader: 'postcss-loader?sourceMap',
 				options: {
 					ident: 'postcss',
-					plugins: [require('postcss-import')(), postcssPresetEnv(postcssPresetConfig)]
+					plugins: [
+						require('postcss-import')(),
+						require('postcss-url')({
+							url: 'rebase'
+						}),
+						postcssPresetEnv(postcssPresetConfig)
+					]
 				}
 			}
 		]
