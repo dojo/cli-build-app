466b1f6d0ad14fc6601dd9abd3434ceed37f2854
Anthony Gubler <agubler@gmail.com>
2018-10-01 04:14:35 -0700

    Add `postcss-url` for rebasing urls (#177)
    * add postcss-url for rebasing urls
    
    * add import

---
+++ tests/integration/relative-css-url.spec.ts
@@ -0,0 +1,23 @@
+describe('relative-css-url', () => {
+	function testUrl(dir: string) {
+		cy.visit(`/test-app/output/${dir}`);
+		cy.get('#div').should(
+			'contain',
+			`Built with Build Time Render: true
+Currently Rendered by BTR: false`
+		);
+		cy.get('i.fab').should('have.css', 'font-family', '"Font Awesome 5 Brands"');
+	}
+	it('dist', () => {
+		testUrl('dist-app');
+		testUrl('dist-app-evergreen');
+		testUrl('dist-pwa');
+		testUrl('dist-pwa-evergreen');
+	});
+	it('dev', () => {
+		testUrl('dev-app');
+		testUrl('dev-app-evergreen');
+		testUrl('dev-pwa');
+		testUrl('dev-pwa-evergreen');
+	});
+});
