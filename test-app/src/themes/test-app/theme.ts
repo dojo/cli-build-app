2f4898678b32c54e947858543fb8347e8a301c45
Anthony Gubler <agubler@gmail.com>
2018-10-26 14:08:19 +0100

    process external m.css files (#191)

---
+++ test-app/src/themes/test-app/theme.ts
@@ -0,0 +1,7 @@
+import * as enhancedTextInput from '@dojo/themes/dojo/enhanced-text-input.m.css';
+import * as listbox from '@dojo/themes/dojo/listbox.m.css';
+
+export default {
+	'@dojo/widgets/enhanced-text-input': enhancedTextInput,
+	'@dojo/widgets/listbox': listbox
+};
