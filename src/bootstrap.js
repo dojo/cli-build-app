462075635aba9c33673f9fed6a32456b6cde0972
Anthony Gubler <agubler@sitepen.com>
2018-11-27 23:39:23 +0000

    make bootstrap compatible with IE11

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,13 +1,13 @@
 import has, { add, exists } from '@dojo/framework/has/has';
 import global from '@dojo/framework/shim/global';
+import '@dojo/framework/shim/Promise';
 
 if (!exists('build-time-render')) {
 	add('build-time-render', false, false);
 }
 
 if (global.__public_path__) {
-	// @ts-ignore
-	__webpack_public_path__ = `${window.location.origin}${global.__public_path__}`;
+	__webpack_public_path__ = window.location.origin + global.__public_path__;
 	add('public-path', global.__public_path__, true);
 }
 
@@ -29,6 +29,6 @@ if (!has('dom-pointer-events')) {
 	modules.push(import(/* webpackChunkName: "platform/pointerEvents" */ '@dojo/framework/shim/pointerEvents'));
 }
 
-Promise.all(modules).then(() => {
+Promise.all(modules).then(function () {
 	import(/* webpackChunkName: "main" */ __MAIN_ENTRY);
 });
