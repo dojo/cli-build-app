648b10eae911ae2d65d4e052aa714b329173396e
Anthony Gubler <agubler@gmail.com>
2019-01-24 17:11:21 +0000

    try to serve static before rewrites and also serve after rewrites (#236)

--- tests/unit/main.ts
+++ tests/unit/main.ts
@@ -466,7 +466,7 @@ describe('command', () => {
 					watch: true
 				})
 				.then(() => {
-					assert.strictEqual(useStub.callCount, 4);
+					assert.strictEqual(useStub.callCount, 5);
 					assert.isTrue(
 						hotMiddleware.calledWith(compiler, {
 							heartbeat: 10000
