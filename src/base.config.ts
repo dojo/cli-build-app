1be77cdc83fce44632555736d83bfaeababf232c
Anthony Gubler <agubler@sitepen.com>
2019-09-09 15:51:56 +0100

    load css from external source but do not process css modules

--- src/base.config.ts
+++ src/base.config.ts
@@ -617,12 +617,7 @@ export default function webpackConfigFactory(args: any): webpack.Configuration {
 				},
 				{
 					test: /\.css$/,
-					exclude: (path: string) => {
-						if (path.indexOf(srcPath) > -1 || path.indexOf(testPath) > -1) {
-							return true;
-						}
-						return /\.m\.css$/.test(path) && !/.*(\/|\\)node_modules(\/|\\).*/.test(path);
-					},
+					include: /.*(\/|\\)node_modules(\/|\\).*/,
 					use: [
 						MiniCssExtractPlugin.loader,
 						{
