dojo/cli-build-app

commit 1a6db18b26d744dd1cb7b42565288e19e2ef3744

Update compression-webpack-plugin and use it for brotli compression aswell as gzip. (#416)

src/dist.config.ts
@@ -20,7 +20,6 @@ import baseConfigFactory, {
 } from './base.config';
 import { WebAppManifest } from './interfaces';
 
-const BrotliPlugin = require('brotli-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
@@ -169,14 +168,13 @@ function webpackConfig(args: any): webpack.Configuration {
 	});
 
 	if (Array.isArray(args.compression)) {
-		const compressionPlugins: any = {
-			gzip: CompressionPlugin,
-			brotli: BrotliPlugin
-		};
 		args.compression.forEach((algorithm: 'brotli' | 'gzip') => {
-			const options = { algorithm, test: /\.(js|css|html|svg)$/ };
-			const Plugin = compressionPlugins[algorithm];
-			config.plugins!.push(new Plugin(options));
+			config.plugins!.push(
+				new CompressionPlugin({
+					algorithm: algorithm === 'brotli' ? 'brotliCompress' : 'gzip',
+					test: /\.(js|css|html|svg)$/
+				})
+			);
 		});
 	}
 

commit 578f022c9f288ce0d999e1e06d04711436b70a78

Fix bugs introduced in #416 (#419)
* Fix brotli compression to output correct suffix.

* Compressed files reported with blue size, and gzipped size not re-calculated.

src/dist.config.ts
@@ -170,10 +170,19 @@ function webpackConfig(args: any): webpack.Configuration {
 	if (Array.isArray(args.compression)) {
 		args.compression.forEach((algorithm: 'brotli' | 'gzip') => {
 			config.plugins!.push(
-				new CompressionPlugin({
-					algorithm: algorithm === 'brotli' ? 'brotliCompress' : 'gzip',
-					test: /\.(js|css|html|svg)$/
-				})
+				new CompressionPlugin(
+					algorithm === 'brotli'
+						? {
+								filename: '[path].br[query]',
+								algorithm: 'brotliCompress',
+								test: /\.(js|css|html|svg)$/,
+								cache: false
+						  }
+						: {
+								test: /\.(js|css|html|svg)$/,
+								cache: false
+						  }
+				)
 			);
 		});
 	}

src/logger.ts
@@ -29,9 +29,13 @@ export default function logger(stats: any, config: any, runningMessage: string =
 				} else {
 					const fileStats = fs.statSync(filePath);
 					const size = (fileStats.size / 1000).toFixed(2);
-					const assetInfo = `${assetName} ${chalk.yellow(`(${size}kb)`)}`;
+					if (/\.(gz|br)$/.test(filePath)) {
+						return `${assetName} ${chalk.blue(`(${size}kb)`)}`;
+					}
+					// Calculate and report size when gzipped
 					const content = fs.readFileSync(filePath, 'utf8');
 					const compressedSize = (gzipSize.sync(content) / 1000).toFixed(2);
+					const assetInfo = `${assetName} ${chalk.yellow(`(${size}kb)`)}`;
 					return `${assetInfo} / ${chalk.blue(`(${compressedSize}kb gz)`)}`;
 				}
 			}