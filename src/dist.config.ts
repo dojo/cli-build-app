588dddb2a4e9d9b96c53440170921376c8dd4b65
Bradley Maier <bmaier@sitepen.com>
2020-08-17 09:44:54 -0700

    Remove public path option from dist config for files

--- src/dist.config.ts
+++ src/dist.config.ts
@@ -174,9 +174,7 @@ function webpackConfig(args: any): webpack.Configuration {
 					...rule,
 					loader: args.omitHash
 						? rule.loader
-						: `file-loader?publicPath=${
-								args.base === undefined ? '/' : args.base
-						  }&hash=sha512&digest=hex&name=[path][name].[hash:base64:8].[ext]`
+						: `file-loader?hash=sha512&digest=hex&name=[path][name].[hash:base64:8].[ext]`
 				};
 			}
 
